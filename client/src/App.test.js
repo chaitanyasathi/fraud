7ba9cbf73338c2710648f27b9338ac1cc03ed9d5
Chaitanya Sathi <chaitanya.sathi@outlook.com>
2018-11-26 11:22:37 -0500

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,3 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Component } from "react";
+import { Grid, Row, Col, Clearfix, Button, Panel } from "react-bootstrap";
+
+it('renders without crashing', () => {
+
+  });
